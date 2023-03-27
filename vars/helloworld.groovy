fde0bc491bba6f3687de6f72f1892377d0a6dca4
manojsubramaniam <58427573+manojsubramaniam@users.noreply.github.com>
2023-03-27 11:08:29 +0530

    Update helloworld.groovy

--- vars/helloworld.groovy
+++ vars/helloworld.groovy
@@ -1,2 +1,3 @@
-import static liveplugin.PluginUtil.show
-show("Hello world") // Shows balloon notification popup with "Hello world" text
+def call(Map config = [:]) {
+    sh "echo Hello ${config.name}. Today is ${config.dayOfWeek}."
+}
